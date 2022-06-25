853cdb96ba799a516f70087c5194e39c95be1f44
aparus <rustamapaev@gmail.com>
2022-06-25 17:15:14 +0300

    6.2. rewrite langSwitcher with props

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import LangSwitcher from './components/LangSwitcher.js'
 import Keyboard from './components/Keyboard.js'
+import LangSwitcher from './components/LangSwitcher.js'
 
 const App = {
 	template: `App 
-	<vue-lang-switcher />
+	<vue-lang-switcher :langs="langs" />
 	<vue-keyboard />
 	`,
 	components: {
 		'vue-lang-switcher': LangSwitcher,
 		'vue-keyboard': Keyboard
+	},
+	/* add: */
+	data() {
+		return {
+			langs: ['en', 'ru', 'ar']
+		}
 	}
 }
 
--- components/LangSwitcher.js
+++ components/LangSwitcher.js
@@ -1,5 +1,17 @@
 const LangSwitcher = {
-	template: `<div class="langSwitcher">LangSwitcher</div>`
+	template: `
+    <div class="langSwitcher">
+        <div 
+            v-for="lang in langs" 
+            class="lang"
+        >
+            {{lang}}
+        </div>
+    </div>`,
+	/* add: */
+	props: {
+		langs: Array
+	}
 }
 
 export default LangSwitcher
