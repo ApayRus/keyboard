6f7233d1a688a09914558e876c9d7b1b19e50307
aparus <rustamapaev@gmail.com>
2022-06-25 14:29:22 +0300

    5. Modularity 2. Passing props. Computed template variables

--- components/Key.js
+++ components/Key.js
@@ -1,5 +1,46 @@
+const getKeyLabels = keyContent => {
+	/*
+	Code line below is called `destructuring`, 
+	because we destructure an object `this.keyContent` 
+	into 3 separate constants.
+	*/
+	const { main = '', shifted = '', label, code } = keyContent
+	const isUpperCaseLang = main.toUpperCase() === shifted
+	const mainOutput = isUpperCaseLang ? shifted : main
+	const shiftedOutput = isUpperCaseLang ? '' : shifted
+
+	return {
+		/*
+		|| is a logical `or` operator. 
+		The line below is `or chain`. 
+		If `label` exists, it will be returned. 
+		If `label` doesn't exist, but `main` exists -- will be returned `main`. 
+		If `label` and main don't exist, will be returned `code`.
+		*/
+		main: label || mainOutput || code,
+		shifted: shiftedOutput
+	}
+}
+
 const Key = {
-	template: `<div class="key">Key</div>`
+	template: `
+    <div class="key">
+        <div class="main">{{main}}</div>
+        <div class="shifted">{{shifted}}</div>
+    </div>`,
+	props: {
+		keyContent: Object
+	},
+	computed: {
+		main() {
+			const { main } = getKeyLabels(this.keyContent)
+			return main
+		},
+		shifted() {
+			const { shifted } = getKeyLabels(this.keyContent)
+			return shifted
+		}
+	}
 }
 
 export default Key
--- components/Keyboard.js
+++ components/Keyboard.js
@@ -1,14 +1,38 @@
 import Key from './Key.js'
+import keyboardData from '../keyboardData/en.js'
 
 const Keyboard = {
-	template: `<div class="keyboard">
-                    Keyboard
-                    <vue-key />
-                    <vue-key />
-                    <vue-key />
-                </div>`,
+	template: `
+    <div class="keyboard">
+        <div class="row row-1">
+            <vue-key :keyContent="keyboardData[0][0]" />
+            <vue-key :keyContent="keyboardData[0][1]" />
+            <vue-key :keyContent="keyboardData[0][2]" />
+            <vue-key :keyContent="keyboardData[0][3]" />
+            <vue-key :keyContent="keyboardData[0][4]" />
+            <vue-key :keyContent="keyboardData[0][5]" />
+        </div>
+        <div class="row row-2">
+            <vue-key :keyContent="keyboardData[1][0]" />
+            <vue-key :keyContent="keyboardData[1][1]" />
+            <vue-key :keyContent="keyboardData[1][2]" />
+            <vue-key :keyContent="keyboardData[1][3]" />
+            <vue-key :keyContent="keyboardData[1][4]" />
+            <vue-key :keyContent="keyboardData[1][5]" />
+        </div>
+        <div class="row row-3">
+            <vue-key :keyContent="keyboardData[2][0]" />
+            <vue-key :keyContent="keyboardData[2][1]" />
+            <vue-key :keyContent="keyboardData[2][2]" />
+            <vue-key :keyContent="keyboardData[2][3]" />
+            <vue-key :keyContent="keyboardData[2][4]" />
+        </div>
+    </div>`,
 	components: {
 		'vue-key': Key
+	},
+	data() {
+		return { keyboardData }
 	}
 }
 
