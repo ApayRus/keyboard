294b4f38fdf38b4192458fa9bddb91a5e8583b5f
aparus <rustamapaev@gmail.com>
2022-06-26 03:20:40 +0300

    7.1 Reactive state, @click event, method call

--- components/LangSwitcher.js
+++ components/LangSwitcher.js
@@ -1,16 +1,31 @@
 const LangSwitcher = {
 	template: `
-    <div class="langSwitcher">
-        <div 
-            v-for="lang in langs" 
-            class="lang"
-        >
-            {{lang}}
-        </div>
-    </div>`,
-	/* add: */
+	<div class="langSwitcher">
+		<div 
+			v-for="lang in langs" 
+			:class='["lang", {active: currentLang === lang}]'
+			@click="switchLang(lang)"
+		>
+			{{lang}}
+		</div>
+    </div>
+	<div style="text-align: center;">
+		{{currentLang}}
+	</div>`,
 	props: {
 		langs: Array
+	},
+	/* add: */
+	data() {
+		return {
+			currentLang: 'en'
+		}
+	},
+	/* add: */
+	methods: {
+		switchLang(lang) {
+			this.currentLang = lang
+		}
 	}
 }
 
